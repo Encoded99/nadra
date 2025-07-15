3cf181a846f70705d8898842aa93313e12109b5e
Ismail Umar <119731277+Encoded99@users.noreply.github.com>
2025-07-15 21:10:00 +0100

    Add files via upload

---
+++ src/app/reusable-components/special/special.tsx
@@ -0,0 +1,23 @@
+'use client'
+import React,{useState} from 'react'
+
+import Image from 'next/image'
+
+
+export const Logo=({width,height}:{width:string,height:string})=>{
+
+
+
+  return (
+    <>
+      <div className={`h-${height} w-${width} relative`}>
+ <Image
+      alt='production'
+       src="/images/24e4e505e38beb6cf65fc7a6db6cbac828afffdb.png"
+         fill
+    
+      />
+      </div>
+    </>
+  )
+}
